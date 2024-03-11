ecf7ac7654883a8593f146f1831e17b2b315e499
Meng <jaymengsniper@gmail.com>
2024-03-11 17:34:57 +0800

    feat: send amount with scan address qr code

--- app/src/main/java/com/flowfoundation/wallet/page/scan/Utils.kt
+++ app/src/main/java/com/flowfoundation/wallet/page/scan/Utils.kt
@@ -4,10 +4,16 @@ import android.app.Activity
 import android.content.Context
 import android.net.Uri
 import android.webkit.URLUtil
+import com.flowfoundation.wallet.manager.coin.FlowCoin
 import com.flowfoundation.wallet.manager.config.AppConfig
+import com.flowfoundation.wallet.manager.wallet.WalletManager
 import com.flowfoundation.wallet.manager.walletconnect.WalletConnect
+import com.flowfoundation.wallet.network.model.AddressBookContact
 import com.flowfoundation.wallet.page.browser.openBrowser
+import com.flowfoundation.wallet.page.send.transaction.subpage.amount.SendAmountActivity
+import com.flowfoundation.wallet.utils.addressPattern
 import com.flowfoundation.wallet.utils.logd
+import com.flowfoundation.wallet.wallet.toAddress
 
 
 fun dispatchScanResult(context: Context, str: String) {
@@ -25,6 +31,11 @@ fun dispatchScanResult(context: Context, str: String) {
         } ?: return
         logd("wc", "wcUri: $wcUri")
         WalletConnect.get().pair(wcUri)
+    } else if (addressPattern.matches(text)) {
+        if (WalletManager.isChildAccountSelected()) {
+            return
+        }
+        SendAmountActivity.launch(context as Activity, AddressBookContact(address = text.toAddress()), FlowCoin.SYMBOL_FLOW)
     } else if (URLUtil.isValidUrl(text.httpPrefix())) {
         openBrowser(context as Activity, text.httpPrefix())
     }
