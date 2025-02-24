5efcf5b925c874fa54c8b122de4667baa2c0c2f0
jaymengxy <jaymengsniper@gmail.com>
2025-02-24 13:44:58 +0800

    fix: landing page ui

--- app/src/main/java/com/flowfoundation/wallet/page/landing/LandingActivity.kt
+++ app/src/main/java/com/flowfoundation/wallet/page/landing/LandingActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import com.flowfoundation.wallet.R
 import com.flowfoundation.wallet.base.activity.BaseActivity
 import com.flowfoundation.wallet.databinding.ActivityLandingBinding
 import com.flowfoundation.wallet.manager.account.OnWalletDataUpdate
@@ -12,10 +11,7 @@ import com.flowfoundation.wallet.manager.account.WalletFetcher
 import com.flowfoundation.wallet.mixpanel.MixpanelManager
 import com.flowfoundation.wallet.network.model.WalletListData
 import com.flowfoundation.wallet.page.backup.WalletBackupActivity
-import com.flowfoundation.wallet.page.landing.utils.AutoScrollViewPager
 import com.flowfoundation.wallet.utils.extensions.gone
-import com.flowfoundation.wallet.utils.extensions.invisible
-import com.flowfoundation.wallet.utils.extensions.res2color
 import com.flowfoundation.wallet.utils.extensions.visible
 import com.flowfoundation.wallet.utils.ioScope
 import com.zackratos.ultimatebarx.ultimatebarx.UltimateBarX
@@ -23,7 +19,6 @@ import com.zackratos.ultimatebarx.ultimatebarx.UltimateBarX
 
 class LandingActivity: BaseActivity(), OnWalletDataUpdate {
     private lateinit var binding: ActivityLandingBinding
-    private var autoScroll: AutoScrollViewPager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,30 +45,22 @@ class LandingActivity: BaseActivity(), OnWalletDataUpdate {
     private fun setupButton(isLoading: Boolean = true) {
         with(binding) {
             if (isLoading) {
-                flLandingDone.gone()
-                tabLayout.visible()
+                clLandingDone.gone()
                 clButton.gone()
-                pbButtonLoading.visible()
                 tvTips.visible()
             } else {
                 MixpanelManager.accountCreationFinish()
-                autoScroll?.stopAutoScroll()
                 lottieAnimation.gone()
                 tvCreatingWallet.gone()
                 tvSecureDesignText.gone()
                 tvSecureDesign.gone()
-                flLandingDone.visible()
+                clLandingDone.visible()
                 clButton.visible()
-                tabLayout.gone()
-                clButton.setBackgroundResource(R.drawable.bg_landing_step_done)
                 clButton.setOnClickListener {
                     WalletBackupActivity.launch(this@LandingActivity, fromRegistration = true)
                     finish()
                 }
-                tvButtonTitle.setText(R.string.landing_step_done)
-                tvButtonTitle.setTextColor(R.color.white_90.res2color())
-                pbButtonLoading.gone()
-                tvTips.invisible()
+                tvTips.gone()
             }
         }
     }
