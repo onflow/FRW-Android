2d4af16bfb511140db01a5c701f75e77f1c68ea6
Lea Lobanov <lea.lobanov@icloud.com>
2025-06-09 22:31:49 +0900

    Re-enable tests

--- app/src/androidTest/java/com/flowfoundation/wallet/TestApi.kt
+++ app/src/androidTest/java/com/flowfoundation/wallet/TestApi.kt
@@ -15,58 +15,71 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import wallet.core.jni.CoinType
 import wallet.core.jni.HDWallet
+import com.flow.wallet.keys.PrivateKey
+import com.flow.wallet.storage.FileSystemStorage
+import com.flowfoundation.wallet.utils.Env
+import org.onflow.flow.models.SigningAlgorithm
+import java.io.File
 
 @RunWith(AndroidJUnit4::class)
 class TestApi {
 
-//    @Test
-//    fun testRegister() {
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val wallet = HDWallet("normal dune pole key case cradle unfold require tornado mercy hospital buyer", "")
-//            val privateKey = wallet.getDerivedKey(CoinType.FLOW, 0, 0, 0)
+    @Test
+    fun testRegister() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val wallet = HDWallet("normal dune pole key case cradle unfold require tornado mercy hospital buyer", "")
+            val privateKey = wallet.getDerivedKey(CoinType.ETHEREUM, 0, 0, 0)
+            val publicKey = privateKey.publicKeyNist256p1.uncompressed().data().bytesToHex().removePrefix("04")
+
+            val deviceInfoRequest = DeviceInfoManager.getDeviceInfoRequest()
+            val service = retrofit().create(ApiService::class.java)
+            val user = service.register(
+                RegisterRequest(
+                    username = "ttt",
+                    accountKey = AccountKey(publicKey = publicKey),
+                    deviceInfo = deviceInfoRequest
+                )
+            )
+            Log.w("user", user.toString())
+        }
+    }
+
+    @Test
+    fun testKeyStoreRegister() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val wallet = HDWallet("normal dune pole key case cradle unfold require tornado mercy hospital buyer", "")
+//            val privateKey = wallet.getDerivedKey(CoinType.ETHEREUM, 0, 0, 0)
 //            val publicKey = privateKey.publicKeyNist256p1.uncompressed().data().bytesToHex().removePrefix("04")
-//
-//            val deviceInfoRequest = DeviceInfoManager.getDeviceInfoRequest()
-//            val service = retrofit().create(ApiService::class.java)
-//            val user = service.register(
-//                RegisterRequest(
-//                    username = "ttt",
-//                    accountKey = AccountKey(publicKey = publicKey),
-//                    deviceInfo = deviceInfoRequest
-//                )
-//            )
-//            Log.w("user", user.toString())
-//        }
-//    }
-//
-//    @Test
-//    fun testKeyStoreRegister() {
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val wallet = HDWallet("normal dune pole key case cradle unfold require tornado mercy hospital buyer", "")
-////            val privateKey = wallet.getDerivedKey(CoinType.FLOW, 0, 0, 0)
-////            val publicKey = privateKey.publicKeyNist256p1.uncompressed().data().bytesToHex().removePrefix("04")
-//
-//            val deviceInfoRequest = DeviceInfoManager.getDeviceInfoRequest()
-//            val keyPair = KeyManager.generateKeyWithPrefix("test_user")
-//            val publicKey = keyPair.public.encoded.bytesToHex().removePrefix("04")
-//            val service = retrofit().create(ApiService::class.java)
-//            val user = service.register(
-//                RegisterRequest(
-//                    username = "ttt",
-//                    accountKey = AccountKey(publicKey = publicKey),
-//                    deviceInfo = deviceInfoRequest
-//                )
-//            )
-//            Log.w("user", user.toString())
-//        }
-//    }
-//
-//    @Test
-//    fun testCreateWallet() {
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val service = retrofit().create(ApiService::class.java)
-//            val resp = service.createWallet()
-//            Log.w("resp", resp.toString())
-//        }
-//    }
+
+            val deviceInfoRequest = DeviceInfoManager.getDeviceInfoRequest()
+            
+            // Create storage for the new Flow Wallet Kit
+            val baseDir = File(Env.getApp().filesDir, "wallet_test")
+            val storage = FileSystemStorage(baseDir)
+            
+            // Use the current Flow Wallet Kit PrivateKey implementation
+            val privateKey = PrivateKey.create(storage)
+            val publicKeyBytes = privateKey.publicKey(SigningAlgorithm.ECDSA_P256)
+            val publicKey = publicKeyBytes?.bytesToHex()?.removePrefix("04") ?: ""
+            
+            val service = retrofit().create(ApiService::class.java)
+            val user = service.register(
+                RegisterRequest(
+                    username = "ttt",
+                    accountKey = AccountKey(publicKey = publicKey),
+                    deviceInfo = deviceInfoRequest
+                )
+            )
+            Log.w("user", user.toString())
+        }
+    }
+
+    @Test
+    fun testCreateWallet() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val service = retrofit().create(ApiService::class.java)
+            val resp = service.createWallet()
+            Log.w("resp", resp.toString())
+        }
+    }
 }
