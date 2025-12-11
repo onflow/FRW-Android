d96068b900fb4070567a3da2fc7a90879aefdf5b
Lea Lobanov <lea.lobanov@icloud.com>
2025-12-11 10:46:09 +0900

    fix: restore SerialName annotations for kotlinx.serialization

--- app/src/main/java/com/flowfoundation/wallet/manager/emoji/model/WalletEmojiInfo.kt
+++ app/src/main/java/com/flowfoundation/wallet/manager/emoji/model/WalletEmojiInfo.kt
@@ -1,10 +1,14 @@
 package com.flowfoundation.wallet.manager.emoji.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class WalletEmojiInfo(
+    @SerialName("address")
     val address: String,
+    @SerialName("emojiId")
     val emojiId: Int,
+    @SerialName("emojiName")
     val emojiName: String
 )
